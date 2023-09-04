13f33dd9cfa6516e57c0a6cc2cd3bec419dd8b4f
ASUS <you@example.com>
2023-09-04 15:07:27 +0300

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,12 @@ import Home from "./Home"
 import { Routes, Route, NavLink } from "react-router-dom";
 import LogIn from "./Login";
 import About from "./About";
+import NavBar from "./NavBar";
 
 
 const App = () => {
     return ( <div>
+        <NavBar/>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
